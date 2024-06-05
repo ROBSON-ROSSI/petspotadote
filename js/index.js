6a14ba767c57d7853a5d249852d10d01a413c796
Robson <robson.rleite@senacsp.edu.br>
2024-06-05 20:07:18 -0300

    master

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var galeria = document.querySelector('.galeria-adoçao');
+var btn_card = document.querySelector('.btn-card');
+
+
+btn_card.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  alert('Botão clicado')
+})
