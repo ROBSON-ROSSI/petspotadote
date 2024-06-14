56ee3077cd0615d3c89eb20ce77ecba87c31186f
Robson <robson_blockar@hotmail>
2024-06-14 16:55:09 -0300

    Criado a função dos banner

--- js/index.js
+++ js/index.js
@@ -1,9 +1,39 @@
-var galeria = document.querySelector('.galeria-adoçao');
+var btn_voltar = document.getElementById('bt-voltar');
+var btn_next = document.getElementById('bt-next');
 var btn_card = document.querySelector('.btn-card');
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+var img3 = document.getElementById('img3');
+var img4 = document.getElementById('img4');
+var marg = 0; 
+btn_voltar.addEventListener('click', () => {
 
-
-btn_card.addEventListener('click', (e) => {
-  e.preventDefault();
+  if(marg == -1920) {
+    btn_voltar.style.visibility = 'hidden';
+    marg = marg + 1920;
+    img1.style.marginLeft = `${marg}`+'px';
+    img1.style.transition = '1s';
+  } else {
   
-  alert('Botão clicado')
+  marg = marg + 1920;
+  img1.style.marginLeft = `${marg}`+'px';
+  img1.style.transition = '1s';
+  btn_next.style.visibility = 'visible';
+  }
+});
+
+btn_next.addEventListener('click', () => {
+  if(marg == -3840){
+    btn_next.style.visibility = 'hidden';
+    marg = marg - 1920;
+    img1.style.marginLeft = `${marg}`+'px';
+    img1.style.transition = '1s';
+  
+  } else {
+  btn_voltar.style.visibility = 'visible';
+  marg = marg - 1920;
+  img1.style.marginLeft = `${marg}`+'px';
+  img1.style.transition = '1s';
+  //alert(marg);
+  }
 })
